1edac9c164a0895c6f2449bf216e59763e6acc53
Jinwoo Jeong <mail@jinwoojeo.ng>
2026-09-06 17:31:49 +0900

    nvim theme rose-pine

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -93,17 +93,11 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "rose-pine/neovim",
+    name = "rose-pine",
     config = function()
-      require('github-theme').setup({
-        -- ...
-      })
-
-      vim.cmd('colorscheme github_dark_default')
-    end,
+      vim.cmd("colorscheme rose-pine")
+    end
   },
   {
     'neovim/nvim-lspconfig',
